86d9c7d766f52074b97d8f31dd62130d50869101
Chris Sullivan <chrissullivan.dev@gmail.com>
2020-01-16 13:50:20 -0800

    Update templates.js
    Fixing assignment, was thinking in python :/

--- src/hbs/node/templates.js
+++ src/hbs/node/templates.js
@@ -12,74 +12,74 @@ const after = require('../{{dir}}').{{name}};
 module.exports.default = async (event, context) => {
   {{#flow 'inline-na'}}
   await before(event, context);
-  response = await handler(event, context);
+  const response = await handler(event, context);
   {{/flow}}
 
   {{#flow 'inline-inline'}}
   await before(event, context);
-  response = await handler(event, context);
+  const response = await handler(event, context);
   await after(event, context);
   {{/flow}}
 
   {{#flow 'inline-wrap'}}
   await before(event, context);
-  response = await handler(after)(event, context);
+  const response = await handler(after)(event, context);
   {{/flow}}
 
   {{#flow 'inline-pipe'}}
   await before(event, context);
-  response = await after(await handler(event, context));
+  const response = await after(await handler(event, context));
   {{/flow}}
 
   {{#flow 'pipe-na'}}
-  response = await handler(await before(event, context));
+  const response = await handler(await before(event, context));
   {{/flow}}
 
   {{#flow 'pipe-inline'}}
-  response = await handler(await before(event, context));
+  const response = await handler(await before(event, context));
   await after(event, context);
   {{/flow}}
 
   {{#flow 'pipe-pipe'}}
-  response = await after(await handler(await before(event, context)));
+  const response = await after(await handler(await before(event, context)));
   {{/flow}}
 
   {{#flow 'pipe-wrap'}}
-  response = await handler(after)(await before(event, context));
+  const response = await handler(after)(await before(event, context));
   {{/flow}}
 
   {{#flow 'na-na'}}
-  response = await handler(event, context);
+  const response = await handler(event, context);
   {{/flow}}
 
   {{#flow 'na-inline'}}
-  response = await handler(event, context);
+  const response = await handler(event, context);
   await after(event, context);
   {{/flow}}
 
   {{#flow 'na-pipe'}}
-  response = await after(await handler(event, context));
+  const response = await after(await handler(event, context));
   {{/flow}}
 
   {{#flow 'na-wrap'}}
-  response = await handler(after)(event, context);
+  const response = await handler(after)(event, context);
   {{/flow}}
 
   {{#flow 'wrap-na'}}
-  response = await before(handler)(event, context);
+  const response = await before(handler)(event, context);
   {{/flow}}
 
   {{#flow 'wrap-inline'}}
-  response = await before(handler)(event, context);
+  const response = await before(handler)(event, context);
   await after(event, context);
   {{/flow}}
 
   {{#flow 'wrap-pipe'}}
-  response = await after(await before(handler)(event, context));
+  const response = await after(await before(handler)(event, context));
   {{/flow}}
 
   {{#flow 'wrap-wrap'}}
-  response = await before(handler(after))(event, context);
+  const response = await before(handler(after))(event, context);
   {{/flow}}
 
   return response;
